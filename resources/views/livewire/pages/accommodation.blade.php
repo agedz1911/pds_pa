1bbfeb06752f65c8f0eb56aae34801ba0cc9847e
AV Pro <asep.cgeds@gmail.com>
2026-07-22 10:35:54 +0700

    feat: update accommodation section with hotel recommendations and improved layout

--- resources/views/livewire/pages/accommodation.blade.php
+++ resources/views/livewire/pages/accommodation.blade.php
@@ -5,18 +5,28 @@
             <h2 class=" uppercase text-2xl font-bold tracking-wide lg:text-4xl">Accommodation</h2>
         </div>
     </section> --}}
-    <h2 class="md:text-4xl text-xl font-semibold uppercase text-center mt-10"> <span class="text-[#FF47AF]">Accommodation
+    <h2 class="md:text-4xl text-xl font-semibold uppercase text-center mt-10"> <span
+            class="text-[#FF47AF]">Accommodation
         </span></h2>
 
     <section class="px-5 md:px-10 pt-0 pb-10 md:py-20 bg-competition">
-        {{-- <div class="text-center md:text-start mb-5">
-            <h2 class="mb-3 uppercase text-4xl tracking-wide font-bold">Hotel <span class="text-[#262262]">Reservation</span></h2>
-            <p class="text-gray-500">The organizers of the ... congress have secured competitive rates at a variety of
-                hotels near the Venue to accommodate delegates
-                with different budgets and preferences. Hotel reservations will open and are subject
-                to availability. It is advisable to book your preferred hotel as soon as possible
+        <div class="text-center md:text-start mb-5">
+            <h2 class="mb-3 uppercase text-xl tracking-wide font-bold">Hotel <span
+                    class="text-[#FF47AF]">Recommendation</span></h2>
+            <p class="text-gray-500">The organizers of the 3<sup>rd</sup> ASEAN PATHOLOGY Conference 2026 Indonesian
+                Chapter
+                congress have secured various hotels near the event venue to accommodate delegates with diverse budgets
+                and preferences.
+
+                <span class="text-gray-500">Everything is still within the Kuningan/Mega Kuningan area, access is easy
+                    via Jl. DR. Ide Anak Agung
+                    Gde Agung → Jl. Prof. DR. Satrio/Jl. HR. Rasuna Said.
+                </span>
+                <span class="text-gray-500">For public transport, the Jakarta LRT (Rasuna Said
+                    or Setiabudi stations) and ride-hailing online taxis are the fastest options, aside from
+                    walking.</span>
             </p>
-        </div> --}}
+        </div>
 
         <div class="flex flex-wrap justify-evenly gap-2 mb-10">
             @foreach ($accommodations as $accommodation)
@@ -27,8 +37,7 @@
                         class="absolute top-1 right-4 translate-y-5 bg-[#2B3990] rounded-xl text-white px-3 py-1">
                         <p class="text-xs">' . $accommodation->tag . '</p>
                     </span>' : " " !!}
-                    <a href="javascript:void(0)"><img
-                            src="{{ asset('storage/' . $accommodation->image) }}"
+                    <a href="javascript:void(0)"><img src="{{ asset('storage/' . $accommodation->image) }}"
                             alt="{{$accommodation->hotel_name}}" class="w-full object-cover rounded-xl"></a>
                 </div>
                 <div class="card-body text-center">
@@ -42,30 +51,30 @@
                                 @endif
                                 @endfor
                         </div>
-                        <p class="mt-2 mb-5 text-xs"><i
-                                class="fa-solid fa-circle-info"></i>
+                        <p class="mt-2 mb-5 text-xs"><i class="fa-solid fa-circle-info"></i>
                             {{$accommodation->distance}}
                         </p>
                         <p class="text-sm"> Cost/Night</p>
                         <div class="text-sm mb-6 flex justify-center gap-3">
-                            <p class="text-[#39B54A] font-semibold mb-0"><span
-                                    class="fw-normal">IDR</span>
+                            <p class="text-[#39B54A] font-semibold mb-0"><span class="fw-normal">IDR</span>
                                 {{number_format($accommodation->idr_price, 0, ',', '.')}}
                             </p>
                             @if ($accommodation->usd_price !== null && $accommodation->usd_price > 0)
-                                
-                            <p class="text-[#39B54A] font-semibold"><span
-                                class="fw-normal">USD</span>
+
+                            <p class="text-[#39B54A] font-semibold"><span class="fw-normal">USD</span>
                                 {{$accommodation->usd_price}}
                             </p>
                             @endif
                         </div>
                     </div>
-                    <div class="grid grid-cols-2 gap-2">
-                        <a href="{{$accommodation->url ? $accommodation->url : 'javascript:void(0)'}}"
-                            class="btn hover:bg-[#FF47AF] bg-[#2B3990] text-white w-full rounded-lg">Book Now</a>
+                    <div class="flex flex-col md:flex-row gap-2 justify-evenly w-full">
+                        @if ($accommodation->url !== null)
+                        <a href="{{$accommodation->url}}"
+                            class="btn hover:bg-[#FF47AF] bg-[#2B3990] text-white md:w-36 rounded-lg">Book Now</a>
+                        @endif
                         <a href="{{$accommodation->direction ? $accommodation->direction : 'javascript:void(0)'}}"
-                            class="btn bg-[#FF47AF] hover:bg-[#2B3990] text-white w-full btn-outline rounded-lg"><i class="fa-solid fa-location-dot mx-1"></i>Direction</a>
+                            class="btn bg-[#FF47AF] hover:bg-[#2B3990] text-white md:w-36 btn-outline border-none rounded-lg"><i
+                                class="fa-solid fa-location-dot mx-1"></i>Direction</a>
                     </div>
                 </div>
             </div>
